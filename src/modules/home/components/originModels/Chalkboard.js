e415903e81d690172044a2bbdffe46f78deaa159
Sergio <sergiocanalesm1@gmail.com>
2023-01-20 01:01:07 -0500

    hf

--- src/modules/home/components/originModels/Chalkboard.js
+++ src/modules/home/components/originModels/Chalkboard.js
@@ -6,7 +6,7 @@ https://gltf.pmnd.rs/
 import React, { useRef, useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-const lectureModelUrl = "models/chalkboard2.glb";
+import lectureModelUrl from "./models/chalkboard2.glb";
 
 export default function Chalkboard(props) {
   const { nodes, materials } = useGLTF(lectureModelUrl);
